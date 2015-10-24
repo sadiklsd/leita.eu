d193dbac9dfb8053b8bfb54994a9b40640e7e522
Nicolas Roitero <nicolas.roitero@odobo.com>
2015-10-24 21:56:09 +0200

    gallery

---
+++ thumb.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+FILES=*.jpg
+for i in $FILES
+do
+echo "Prcoessing image $i ..."
+#convert -define jpeg:size=200x200 $i  -thumbnail 150x150^ -gravity #center -extent 150x150  ../../thumbs/$i
+
+done
