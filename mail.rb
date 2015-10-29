a53c3a592bc8584d49bc4a1e84bc7d5887edb2bc
nicolasroitero <nicolas.roitero@odobo.com>
2015-10-29 22:25:58 +0100

    mail

--- mail.rb
+++ mail.rb
@@ -1,6 +1,16 @@
 require 'pony'
 
-    Pony.mail :to =>'nicolas.roitero@gmail.com',
-    :from => 'contact@leita.eu',
+    Pony.mail({ :to =>'nicolas.roitero@gmail.com',
+    :from => 'sadiklsd@gmail.com',
     :subject => "New mail from",
-    :body => "test"
+    :body => "test",
+    :via_options => {
+   :address              => 'smtp.gmail.com',
+   :port                 => '587',
+   :enable_starttls_auto => true,
+   :user_name            => 'sadiklsd',
+   :password             => '$adiklsd25',
+   :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+   :domain               => "leita.eu" # the HELO domain provided by the client to the server
+ }
+ })
