11066fd887a639fdba5cc9ef7772f163692b9a42
Nicolas Roitero <nicolas.roitero@odobo.com>
2015-10-29 15:19:09 +0100

    contact

--- web.rb
+++ web.rb
@@ -2,22 +2,13 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'mail'
 db = URI.parse('postgres://rccbbtlniowhqb:ArygKC-v7LpmIYDYOwSZ0cdCdk@ec2-54-204-15-48.compute-1.amazonaws.com:5432/dcd604ki6e6j1k')
-Mail.defaults do
-  delivery_method :smtp,     address: 'smtp.sendgrid.net',
-                             port: 587,
-                             domain: 'heroku.com',
-                             user_name: ENV['SENDGRID_USERNAME'],
-                             password: ENV['SENDGRID_PASSWORD'],
-                             authentication: 'plain',
-                             enable_starttls_auto: true
-end
 ActiveRecord::Base.establish_connection(
-  adapter: db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-  host: db.host,
-  username: db.user,
-  password: db.password,
-  database: db.path[1..-1],
-  encoding: 'utf8'
+adapter: db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+host: db.host,
+username: db.user,
+password: db.password,
+database: db.path[1..-1],
+encoding: 'utf8'
 )
 class MyApp < Sinatra::Base
   get '/' do
@@ -27,14 +18,16 @@ class MyApp < Sinatra::Base
     name = params[:name]
     email = params[:email]
     text = params[:text]
-    mail = Mail.deliver do
+    mail = Mail.new do
       to 'nicolas.roitero@gmail.com'
-      from 'contact@leita.eu'
+      from 'Laita-web@leita.eu'
       subject "New mail from #{name}"
       content_type 'text/html; charset=UTF-8'
       body "name: #{name}<br>email: <a href=\"mailto:#{email}\">#{email}</a><br>
       text: #{text}"
     end
-    puts mail
+    mail.delivery_method :sendmail
+    mail.deliver
+
   end
 end
